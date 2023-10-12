fe40f7b2cff7a73bc24dee1407237ac0424639f6
TalonEzio <vuthemanh1707@gmail.com>
2023-10-12 11:14:15 +0700

    Add project files.

---
+++ Client/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace Client
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.InputEncoding = Console.OutputEncoding = Encoding.Unicode;
+            try
+            {
+                Socket socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+
+                IPAddress hostAddress = IPAddress.Loopback;
+                IPEndPoint hostEndPoint = new IPEndPoint(hostAddress, 7777);
+
+                Console.WriteLine("Đang kết nối.....");
+                socket.Connect(hostEndPoint);
+                if (socket.Connected) Console.WriteLine("Kết nối thành công");
+
+                Console.WriteLine("Dữ liệu gửi đi :   ");
+                var unicodeEncoding = new UnicodeEncoding();
+                while (true)
+                {
+                    Console.ForegroundColor = ConsoleColor.Green;
+                    Console.Write("-> Client: ");
+                    string str = Console.ReadLine();
+
+                    if (str != null && str.ToLower() == "quit")
+                    {
+                        Console.ForegroundColor = ConsoleColor.Yellow;
+                        Console.WriteLine($"Đã đóng kết nối tới {hostEndPoint.Address}:{hostEndPoint.Port}");
+                        break;
+                    }
+
+                    if (str != null)
+                    {
+                        byte[] binDataOut = unicodeEncoding.GetBytes(str);
+                        socket.Send(binDataOut, 0, binDataOut.Length, SocketFlags.None);
+                    }
+
+                    byte[] binDataIn = new byte[255];
+                    int k = socket.Receive(binDataIn, 0, 255, SocketFlags.None);
+                    Console.ForegroundColor = ConsoleColor.Cyan;
+                    string msg = unicodeEncoding.GetString(binDataIn, 0, k);
+                    Console.WriteLine("-> Server: " + msg);
+                    Console.ResetColor();
+                }
+
+                socket.Close();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Lỗi..... " + e.StackTrace);
+            }
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Server/Program.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Server
+{
+    internal class Program
+    {
+        private static readonly string[] Words = new string[]
+            { "zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine","Giá trị không đúng" };
+        static async Task Main(string[] args)
+        {
+            Console.InputEncoding = Console.OutputEncoding = Encoding.Unicode;
+
+            try
+            {
+                IPAddress ipAddress = IPAddress.Loopback;
+                var serverPort = 7777;
+
+                Console.WriteLine($"Server nghe ở : ${ipAddress}:{serverPort}");
+
+                TcpListener listener = new TcpListener(ipAddress, serverPort);
+                Console.WriteLine("Đang đợi kết nối từ client.....");
+                listener.Start();
+                while (true)
+                {
+                    await Task.Run(() => HandleClient(listener));
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Lỗi..... " + e);
+            }
+
+            Console.ReadLine();
+        }
+        static async void HandleClient(TcpListener tcpListener)
+        {
+            Socket clientSocket = await tcpListener.AcceptSocketAsync();
+            try
+            {
+
+                Console.WriteLine("Kết nối từ " + clientSocket.RemoteEndPoint);
+                var unicodeEncoding = new UnicodeEncoding();
+                Console.WriteLine("Đợi dữ liệu từ client... ");
+                while (true)
+                {
+                    byte[] binDataIn = new byte[255];
+                    int k = clientSocket.Receive(binDataIn);
+
+                    if (k == 0)
+                    {
+                        Console.ForegroundColor = ConsoleColor.Red;
+                        Console.WriteLine(
+                            $"Kết nối từ {clientSocket.RemoteEndPoint} đã đóng");
+                        clientSocket.Close();
+                        Console.ResetColor();
+                        break;
+                    }
+
+                    string msg = unicodeEncoding.GetString(binDataIn, 0, k);
+                    bool isNumber = int.TryParse(msg, out var number);
+                    string returnMessage;
+                    if (isNumber)
+                    {
+                        var wordIndex = number >= 0 && number <= 9 ? number : Words.Length - 1;
+                        returnMessage = Words[wordIndex];
+                        Console.WriteLine($"Đã gửi dữ liệu cho : {clientSocket.RemoteEndPoint}");
+                    }
+                    else
+                    {
+                        returnMessage = "Dữ liệu đầu vào phải là số, vui lòng thử lại";
+                    }
+
+                    clientSocket.Send(unicodeEncoding.GetBytes(returnMessage));
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Lỗi xử lý client: " + e);
+            }
+            finally
+            {
+                clientSocket.Close();
+                //tcpListener.Stop();
+            }
+        }
+    }
+}
